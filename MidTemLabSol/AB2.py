05a3f75ec5f26f48848c502a8cd2c3bcc3b01357
K N Rastogi <krishrastogi3@gmail.com>
2019-09-18 02:27:34 +0530

    Add files via upload

---
+++ MidTemLabSol/AB2.py
@@ -0,0 +1,18 @@
+##Q2. Write a program to check whether a String is palindrome or not?
+#############################################################################################################
+##Program Objective: to check whether a String is palindrome or not                                        ##
+##Coded by: KNR                                                                                            ##
+##Date: 17/09/2019 22:20                                                                                   ##
+##Lang: Python 3.7.4                                                                                       ##
+##Version: 1.0                                                                                             ##
+#############################################################################################################
+
+print("--------------------------------------------------")
+yourStr = input("Enter your string:")
+if yourStr == yourStr[::-1]:
+    print("{} is a Polindrome".format(yourStr))
+else:
+    print("{} is not a Polindrome".format(yourStr))  
+
+print("--------------------------------------------------")
+
