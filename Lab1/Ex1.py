280e504cde5849c29e81e3dcc1c4ab92e7b92d46
K N Rastogi <krishrastogi3@gmail.com>
2019-09-23 21:50:40 +0530

    Add files via upload

---
+++ Lab1/Ex1.py
@@ -0,0 +1,74 @@
+1)      Write a program to say Hello World on python shell.
+
+>>>print("Hello World!")
+'Hello World!'
+
+2)      Write a program to print your name on python shell.
+
+>>>print("Krish Rastogi")
+'Krish Rastogi'
+
+3)      Write a program to print your address.
+
+print("C/O Mr. A P Rastogi")
+print("#452, Sarvodaya Socity, Alkapuri")
+print("Vadodara, Gujrat-390020")
+
+4)      Do some arithmetic on Python shell with the following operators:
+
++, - ,* ,** , / ,//, %
+
+>>>2+3
+5
+>>>3-1
+2
+>>>2*3
+6
+>>>5**2
+25
+>>>3/2
+1.5
+>>>4//3
+1
+>>>5%3
+2
+>>>a = 6
+>>>b = 9
+>>>a + b
+15
+>>>a - b
+-3
+>>>a * b
+54
+
+5)      Redo Q1-Q3 in python scripts.
+
+6)      Write a program to add two numbers.
+
+a=10
+b=5
+c=a+b
+print("Addition:",c)
+
+
+7)      Write a program to calculate area of a circle.
+
+PI=3.14
+rad=5
+areaofcircle = Pi*rad*rad
+print("Area of circle is:", areaofcircle)
+
+
+8)      Write a program to calculate area of a room.
+
+len=20
+br=10
+area=len*br
+print("Are of a room:", area)
+
+
+9)      Write a program to find sum of first n numbers.
+n=10
+sum= n*(n+1)/2
+print("Sum:", n)
+ 
